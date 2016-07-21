34946b4dc0ff7e92a0bf6012eb30757326d90049
mms <mms@localhost.localdomain>
2016-07-21 17:18:04 +0300

    21

---
+++ src/pag1/Os.java
@@ -0,0 +1,23 @@
+
+package pag;
+
+/**
+ *Операции,арифметические действия с целочислеными числами
+ * 
+ */
+public class Os {
+    public static void main(String args[]){
+        int a=4;
+        int b=2;
+        int c=a*b;
+        int d=a/b;
+        int x=a%b;
+        int i=-a-d;       
+        System.out.println("C="+c);
+        System.out.println("d="+d);
+        System.out.println("x="+x);
+        System.out.println("i="+i);
+        
+    }
+    
+}
---
+++ src/pag1/Os_d.java
@@ -0,0 +1,14 @@
+
+package pag;
+
+/**
+ * Операции,арифметические действия с дествительными числами
+ */
+public class Os_d {
+    public static void main(String args[]){
+        double ad=4.7;
+        float  af=3.314f;
+        System.out.println("Результат "+"\n"+(ad+af)+"\n"+(ad-af)+"\n"+(ad/af)+"\n"+(ad*af)+"\n"+(ad%af));
+    }
+    
+}
