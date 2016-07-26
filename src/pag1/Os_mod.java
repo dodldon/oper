810068e8d72e61242599673e349cfe4160b7c679
mms <mms@localhost.localdomain>
2016-07-26 17:02:27 +0300

    26

---
+++ src/pag1/Os_mod.java
@@ -0,0 +1,17 @@
+package pag1;
+
+/**
+ *
+ * Операции,деления по модулю
+ */
+public class Os_mod {
+
+    public static void main(String args[]) {
+        int x = 13;
+        double d = 12.24;
+
+        System.out.println("X%10=" + x % 10);
+        System.out.println("d%10=" + d % 10);
+
+    }
+}
